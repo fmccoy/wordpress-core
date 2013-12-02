449a875dd8fdfce19155d0da9812dbf060593365
Frank McCoy <fmccoy@thisisamg.com>
2013-12-01 23:19:39 -0600

    _
    Signed-off-by: Frank McCoy <fmccoy@thisisamg.com>

--- local/config-sample.php
+++ local/config-sample.php
@@ -4,6 +4,6 @@
 // Database Connection Info
 // ===================================================
 define( 'DB_NAME', '%%DB_NAME%%' );
-define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_USER', '%%DB_USER%%' );
+define( 'DB_PASSWORD', '%%DB_PASSWORD%%' );
 define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
