afa13f2dc47e7ceac0b6441c83783f95a3b5dea1
Frank McCoy <fmccoy@thisisamg.com>
2013-12-03 13:44:05 -0600

    shouldn't be here
    Signed-off-by: Frank McCoy <fmccoy@thisisamg.com>

--- config-sample.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// ===================================================
-// Database Connection Info
-// ===================================================
-define( 'DB_NAME', '%%DB_NAME%%' );
-define( 'DB_USER', '%%DB_USER%%' );
-define( 'DB_PASSWORD', '%%DB_PASSWORD%%' );
-define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
