8fce2070dec54e69615ca912062ed769efda8e8b
Frank McCoy <fmccoy@thisisamg.com>
2013-12-03 13:39:27 -0600

    sample config file
    Signed-off-by: Frank McCoy <fmccoy@thisisamg.com>

---
+++ config-sample.php
@@ -0,0 +1,9 @@
+<?php
+
+// ===================================================
+// Database Connection Info
+// ===================================================
+define( 'DB_NAME', '%%DB_NAME%%' );
+define( 'DB_USER', '%%DB_USER%%' );
+define( 'DB_PASSWORD', '%%DB_PASSWORD%%' );
+define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
