31a1c3679ca9b8e727b845e5bd3a479d7a795464
Frank McCoy <frankmccoy.d@gmail.com>
2014-01-16 13:06:49 -0600

    update sample config

--- local/config-sample.php
+++ local/config-sample.php
@@ -7,9 +7,9 @@
 /**
  * Database Connection Info
  */
-define( 'DB_NAME', '%%DB_NAME%%' );
-define( 'DB_USER', '%%DB_USER%%' );
-define( 'DB_PASSWORD', '%%DB_PASSWORD%%' );
+define( 'DB_NAME', 'DB_NAME' );
+define( 'DB_USER', 'DB_USER' );
+define( 'DB_PASSWORD', 'DB_PASSWORD' );
 define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
 
 /**
@@ -17,4 +17,12 @@ define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
  * We are developing locally here so lets turn debug on,
  * if not comment out.
  */
-define( 'WP_DEBUG', true );
+//define( 'WP_DEBUG', true );
+
+/**
+ * Content Directory 
+ * 
+ * If wordpress is installed in a subdirectory you need to adjust your
+ * content directory url accordingly.
+ */
+define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
