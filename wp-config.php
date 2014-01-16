6c92e61e5dc7fee47801604e6ab664c91a6c3ef3
Frank McCoy <frankmccoy.d@gmail.com>
2014-01-16 13:07:31 -0600

    moved WP_CONTENT_URL to config.php

--- wp-config.php
+++ wp-config.php
@@ -30,7 +30,6 @@ if ( file_exists( dirname( __FILE__ ) . '/local/config.php' ) ) {
  * Custom Content Directory
  */
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
-define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
 
 /**
  * Database Settings
